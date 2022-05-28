4f269cebb0b5c374de8f751fe2f897629cdc5479
scx567888 <scx567888@outlook.com>
2022-05-29 00:30:49 +0800

    add addTwoNumbers (#3)

---
+++ src/main/java/cool/scx/learn/common/SingleLinkedListNode.java
@@ -0,0 +1,126 @@
+package cool.scx.learn.common;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+
+/**
+ * 单项链表节点
+ *
+ * @param <T>
+ */
+public class SingleLinkedListNode<T> {
+
+    public final T val;
+
+    public final SingleLinkedListNode<T> next;
+
+    public SingleLinkedListNode() {
+        this(null);
+    }
+
+    public SingleLinkedListNode(T val) {
+        this(val, null);
+    }
+
+    public SingleLinkedListNode(T val, SingleLinkedListNode<T> next) {
+        this.val = val;
+        this.next = next;
+    }
+
+
+    /**
+     * 链表转数组 (基本类型 int)
+     *
+     * @param SingleLinkedListNode 链表
+     * @return 数组
+     */
+    public static int[] toIntArray(final SingleLinkedListNode<Integer> SingleLinkedListNode) {
+        return toList(SingleLinkedListNode).stream().mapToInt(c -> c).toArray();
+    }
+
+    /**
+     * 链表转数组 (因为 java 残废的泛型所以这里没法返回泛型数组)
+     *
+     * @param SingleLinkedListNode 链表
+     * @return Object 数组
+     */
+    public static Object[] toArray(final SingleLinkedListNode<?> SingleLinkedListNode) {
+        return toList(SingleLinkedListNode).toArray();
+    }
+
+    /**
+     * 链表转集合
+     *
+     * @param SingleLinkedListNode 链表
+     * @return 数组
+     */
+    public static <T> ArrayList<T> toList(final SingleLinkedListNode<T> SingleLinkedListNode) {
+        var list = new ArrayList<T>();
+        for (var x = SingleLinkedListNode; x != null; x = x.next) {
+            list.add(x.val);
+        }
+        return list;
+    }
+
+    /**
+     * 数组转链表
+     *
+     * @param array 数组
+     * @return 链表
+     */
+    public static <T> SingleLinkedListNode<T> fromArray(final T[] array) {
+        SingleLinkedListNode<T> tempSingleLinkedListNode = null;
+        for (int i = array.length - 1; i >= 0; i--) {
+            tempSingleLinkedListNode = new SingleLinkedListNode<>(array[i], tempSingleLinkedListNode);
+        }
+        return tempSingleLinkedListNode;
+    }
+
+    /**
+     * 数组转链表 (同样因为 java 残废的泛型才有的方法)
+     *
+     * @param array 数组
+     * @return 链表
+     */
+    public static SingleLinkedListNode<Integer> fromIntArray(final int[] array) {
+        return fromArray(Arrays.stream(array).boxed().toArray(Integer[]::new));
+    }
+
+    /**
+     * 数组转链表 (同样因为 java 残废的泛型才有的方法)
+     *
+     * @param array 数组
+     * @return 链表
+     */
+    public static SingleLinkedListNode<Integer> fromIntArray2(final int[] array) {
+        return fromArray2(Arrays.stream(array).boxed().toArray(Integer[]::new));
+    }
+
+    /**
+     * 递归方式的数组转链表
+     *
+     * @param array 数组
+     * @return 链表
+     */
+    public static <T> SingleLinkedListNode<T> fromArray2(final T[] array) {
+        return _fromArray2(null, array, array.length - 1);
+    }
+
+    /**
+     * 真正的递归方法
+     *
+     * @param next  下一个节点
+     * @param array 数组
+     * @param index 当前数组的索引
+     * @return 链表
+     */
+    private static <T> SingleLinkedListNode<T> _fromArray2(SingleLinkedListNode<T> next, T[] array, int index) {
+        var tempSingleLinkedListNode = new SingleLinkedListNode<>(array[index], next);
+        if (index == 0) {
+            return tempSingleLinkedListNode;
+        } else {
+            return _fromArray2(tempSingleLinkedListNode, array, index - 1);
+        }
+    }
+
+}
---
+++ src/main/java/cool/scx/learn/leetcode/AddTwoNumbers.java
@@ -0,0 +1,54 @@
+package cool.scx.learn.leetcode;
+
+import cool.scx.learn.common.SingleLinkedListNode;
+
+/**
+ * 给你两个 非空 的链表，表示两个非负的整数。它们每位数字都是按照逆序的方式存储的，并且每个节点只能存储一位数字。
+ * <p>
+ * 请你将两个数相加，并以相同形式返回一个表示和的链表。
+ * <p>
+ * 你可以假设除了数字 0 之外，这两个数都不会以 0开头。
+ * <p>
+ * 输入：l1 = [2,4,3], l2 = [5,6,4]
+ * 输出：[7,0,8]
+ * 解释：342 + 465 = 807.
+ * 示例 2：
+ * <p>
+ * 输入：l1 = [0], l2 = [0]
+ * 输出：[0]
+ * 示例 3：
+ * <p>
+ * 输入：l1 = [9,9,9,9,9,9,9], l2 = [9,9,9,9]
+ * 输出：[8,9,9,9,0,0,0,1]
+ */
+public class AddTwoNumbers {
+
+    public static SingleLinkedListNode<Integer> addTwoNumbers(SingleLinkedListNode<Integer> l1, SingleLinkedListNode<Integer> l2) {
+        return _addTwoNumbers(l1, l2, 0);
+    }
+
+    /**
+     * 递归操作
+     *
+     * @param l1 l1
+     * @param l2 l2
+     * @param b  上一次计算的溢出数 如 (7 + 5 = 12) 则溢出数为 1
+     * @return 相加后的链表
+     */
+    public static SingleLinkedListNode<Integer> _addTwoNumbers(SingleLinkedListNode<Integer> l1, SingleLinkedListNode<Integer> l2, int b) {
+        if (l1 == null && l2 == null && b == 0) {
+            return null;
+        }
+        int val = b;
+        if (l1 != null) {
+            val = val + l1.val;
+            l1 = l1.next;
+        }
+        if (l2 != null) {
+            val = val + l2.val;
+            l2 = l2.next;
+        }
+        return new SingleLinkedListNode<>(val % 10, _addTwoNumbers(l1, l2, val / 10));
+    }
+
+}
