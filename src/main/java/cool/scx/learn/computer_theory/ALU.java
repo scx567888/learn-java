d6e4a03a04c7b91ec0168c0dcc9da0b77d085e11
scx567888 <scx567888@outlook.com>
2022-06-11 20:03:51 +0800

    1, 添加全加器

--- src/main/java/cool/scx/learn/computer_theory/ALU.java
+++ src/main/java/cool/scx/learn/computer_theory/ALU.java
@@ -3,6 +3,7 @@ package cool.scx.learn.computer_theory;
 
 import java.util.Arrays;
 
+import static cool.scx.learn.computer_theory.LogicGate.or;
 import static cool.scx.learn.computer_theory.Utils.booleanToInt;
 import static cool.scx.learn.computer_theory.Utils.intToBoolean;
 
@@ -12,17 +13,29 @@ import static cool.scx.learn.computer_theory.Utils.intToBoolean;
 public class ALU {
 
     public static void main(String[] args) {
-        System.out.println(Arrays.toString(halfAdder(1, 1)));
-        System.out.println(Arrays.toString(halfAdder(1, 0)));
-        System.out.println(Arrays.toString(halfAdder(0, 1)));
-        System.out.println(Arrays.toString(halfAdder(0, 0)));
+        System.out.println("半加法器:");
+        for (int i = 0; i < 2; i++) {
+            for (int j = 0; j < 2; j++) {
+                printHalfAdder(i, j);
+            }
+        }
+        System.out.println("全加法器:");
+        for (int i = 0; i < 2; i++) {
+            for (int j = 0; j < 2; j++) {
+                for (int k = 0; k < 2; k++) {
+                    printFullAdder(i, j, k);
+                }
+            }
+        }
     }
 
     /**
      * 半加法器 (返回的数组永远是两位 : [进位, 总和] )
      */
     public static boolean[] halfAdder(boolean a, boolean b) {
+        //两者都为 true 则表示需要进位
         var carry = LogicGate.and(a, b);
+        //1 + 1 的和为 0 正好符合异或门结果
         var sum = LogicGate.xor(a, b);
         return new boolean[]{carry, sum};
     }
@@ -34,6 +47,46 @@ public class ALU {
         var booleans = intToBoolean(a, b);
         var result = halfAdder(booleans[0], booleans[1]);
         return booleanToInt(result);
+    }
+
+    /**
+     * 全加法器 (返回的数组永远是两位 : [进位, 总和] )
+     */
+    public static boolean[] fullAdder(boolean a, boolean b, final boolean c) {
+        //先算其中两个数相加
+        var r1 = halfAdder(a, b);
+        var r1Carry = r1[0];
+        var r1Sum = r1[1];
+        //用上一个数的和与下一个数相加
+        var r2 = halfAdder(r1Sum, c);
+        var r2Carry = r2[0];
+        var r2Sum = r2[1];
+        //两次相加中有任意一次进位了就表示需要一次进位
+        var carry = or(r1Carry, r2Carry);
+        return new boolean[]{carry, r2Sum};
+    }
+
+    /**
+     * 全加法器 (返回的数组永远是两位 : [进位, 总和] )
+     */
+    public static int[] fullAdder(int a, int b, int c) {
+        var booleans = intToBoolean(a, b, c);
+        var result = fullAdder(booleans[0], booleans[1], booleans[2]);
+        return booleanToInt(result);
+    }
+
+    /**
+     * 打印半加法器的计算过程
+     */
+    public static void printHalfAdder(int a, int b) {
+        System.out.println(a + " + " + b + " -> " + Arrays.toString(halfAdder(a, b)));
+    }
+
+    /**
+     * 打印全加法器的计算过程
+     */
+    public static void printFullAdder(int a, int b, int c) {
+        System.out.println(a + " + " + b + " + " + c + " -> " + Arrays.toString(fullAdder(a, b, c)));
     }
 
 }
