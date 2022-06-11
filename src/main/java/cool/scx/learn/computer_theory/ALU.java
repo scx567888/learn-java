83a13adc417fee92bbe107b6ece3b6a4586272b1
scx567888 <scx567888@outlook.com>
2022-06-11 20:22:37 +0800

    1, javadoc:fix (#10)

--- src/main/java/cool/scx/learn/computer_theory/ALU.java
+++ src/main/java/cool/scx/learn/computer_theory/ALU.java
@@ -1,27 +1,38 @@
 package cool.scx.learn.computer_theory;
 
-import static cool.scx.learn.computer_theory.LogicGate.or;
+import static cool.scx.learn.computer_theory.LogicGate.*;
 import static cool.scx.learn.computer_theory.Utils.booleanToInt;
 import static cool.scx.learn.computer_theory.Utils.intToBoolean;
 
 /**
  * 模拟 算术逻辑单元
+ *
+ * @author scx567888
+ * @version 1.0.0
  */
 public class ALU {
 
     /**
      * 半加法器 (返回的数组永远是两位 : [进位, 总和] )
+     *
+     * @param a a boolean
+     * @param b a boolean
+     * @return an array of {@link boolean} objects
      */
     public static boolean[] halfAdder(boolean a, boolean b) {
         //两者都为 true 则表示需要进位
-        var carry = LogicGate.and(a, b);
+        var carry = and(a, b);
         //1 + 1 的和为 0 正好符合异或门结果
-        var sum = LogicGate.xor(a, b);
+        var sum = xor(a, b);
         return new boolean[]{carry, sum};
     }
 
     /**
      * 半加法器 [数字版] (返回的数组永远是两位 : [进位, 总和] )
+     *
+     * @param a a int
+     * @param b a int
+     * @return an array of {@link int} objects
      */
     public static int[] halfAdder(int a, int b) {
         var booleans = intToBoolean(a, b);
@@ -31,6 +42,11 @@ public class ALU {
 
     /**
      * 全加法器 (返回的数组永远是两位 : [进位, 总和] )
+     *
+     * @param a a boolean
+     * @param b a boolean
+     * @param c a boolean
+     * @return an array of {@link boolean} objects
      */
     public static boolean[] fullAdder(boolean a, boolean b, final boolean c) {
         //先算其中两个数相加
@@ -48,6 +64,11 @@ public class ALU {
 
     /**
      * 全加法器 (返回的数组永远是两位 : [进位, 总和] )
+     *
+     * @param a a int
+     * @param b a int
+     * @param c a int
+     * @return an array of {@link int} objects
      */
     public static int[] fullAdder(int a, int b, int c) {
         var booleans = intToBoolean(a, b, c);
--- src/main/java/cool/scx/learn/leetcode/LongestSubstringWithoutRepeatingCharacters.java
+++ src/main/java/cool/scx/learn/leetcode/LongestSubstringWithoutRepeatingCharacters.java
@@ -1,11 +1,18 @@
 package cool.scx.learn.leetcode;
 
+/**
+ * <p>LongestSubstringWithoutRepeatingCharacters class.</p>
+ *
+ * @author scx567888
+ * @version 1.0.0
+ */
 public class LongestSubstringWithoutRepeatingCharacters {
 
     /**
      * todo
-     * @param s
-     * @return
+     *
+     * @param s a {@link java.lang.String} object
+     * @return a int
      */
     public static int lengthOfLongestSubstring(String s) {
 
